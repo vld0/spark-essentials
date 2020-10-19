1f2bb418e716f81bb2376eab239d7d7b3f72aead
Catalin VLAD <catalin.vlad@ext.ec.europa.eu>
2020-10-19 17:54:49 +0300

    top domains

--- src/main/java/com/vld0/spark/PageSize.java
+++ src/main/java/com/vld0/spark/PageSize.java
@@ -1,34 +1,31 @@
 package com.vld0.spark;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.spark.SparkEnv;
 import org.apache.spark.api.java.function.MapFunction;
+
+import java.io.IOException;
 
 public class PageSize implements MapFunction<String, Long> {
 
     @Override
-    public Long call(String url) throws Exception {
-        HttpGet get = new HttpGet(url);
-
-        HttpGet httpget = new HttpGet("http://localhost/");
-
+    public Long call(String url) {
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        CloseableHttpResponse response = httpclient.execute(httpget);
-        /*try {
+        HttpGet get = new HttpGet(url);
+        try (CloseableHttpResponse response = httpclient.execute(get)) {
             HttpEntity entity = response.getEntity();
             if (entity != null) {
-                InputStream instream = entity.getContent();
-                try {            // do something useful        } finally {            instream.close();        }    }} finally {    response.close();}
-
-
-                    return null;
-                }
+                byte[] content = IOUtils.toByteArray(entity.getContent());
+                //System.out.println(Strings.getString(content, Charset.defaultCharset().toString()));
+                return (long)content.length;
             }
+        } catch (IOException e) {
+            return -1l;
         }
-*/
 
         return -1l;
     }
