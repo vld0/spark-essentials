4f303da0e7e2a8a7d7fdafa249338f63510f928f
Catalin VLAD <catalin.vlad@ext.ec.europa.eu>
2020-10-20 15:06:16 +0300

    working version with UDF

--- src/main/scala/vld0/TopDomains.scala
+++ src/main/scala/vld0/TopDomains.scala
@@ -2,7 +2,7 @@ package vld0
 
 import com.creanga.sparktest.LazyLogging
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.{col, udf}
 
 object TopDomains extends LazyLogging {
 
@@ -19,12 +19,10 @@ object TopDomains extends LazyLogging {
     //topDomainsDF.show()
 
     import spark.implicits._
+    //topDomainsDF.select(col("Root Domain")).map( row => PageSize.call(row.getString(0)) ).show();
 
-    topDomainsDF.select(col("Root Domain")).map( row => PageSize.call(row.getString(0)) ).show();
-
-    //TODO cum fac aici?
-    //topDomainsDF.select(col("Root Domain"))
-      //.withColumn("Size", pageSize.call( col("Root Domain")) ).show();
+    val calculatePageSize = udf( (url: String) => PageSize.call(url) )
+    topDomainsDF.select(col("Root Domain")).withColumn("Size", calculatePageSize( col("Root Domain")) ).show();
 
 
 
